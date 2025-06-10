57ac5ef63959b231e07dfffa217a7c0495094d20
smj1Grib <smj1@grib.co.kr>
2025-06-10 16:45:53 +0900

    [추가] 프로젝트 파일 추가(1)

--- src/main/java/kr/co/grib/drools/api/rules/service/Impl/RuleServiceImpl.kt
+++ src/main/java/kr/co/grib/drools/api/rules/service/Impl/RuleServiceImpl.kt
@@ -1,8 +1,16 @@
 package kr.co.grib.drools.api.rules.service.Impl
 
+import kr.co.grib.drools.api.base.dto.BaseCtlDto
+import kr.co.grib.drools.api.rules.dto.RuleRequestDto
 import kr.co.grib.drools.api.rules.service.RuleService
 import org.springframework.stereotype.Service
 
 @Service
 class RuleServiceImpl : RuleService{
+
+    override
+    fun doGetRules(req: RuleRequestDto) :BaseCtlDto{
+        val rtn = BaseCtlDto();
+        return rtn;
+    }
 }
