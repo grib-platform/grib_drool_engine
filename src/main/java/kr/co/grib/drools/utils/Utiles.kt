0f4af70c7d3ac46dae740d4156dc0efedbe712f7
smj1Grib <smj1@grib.co.kr>
2025-06-13 18:08:12 +0900

    [수정/추가] Thymeleaf / drools 확인용 설정 추가

--- src/main/java/kr/co/grib/drools/utils/Utiles.kt
+++ src/main/java/kr/co/grib/drools/utils/Utiles.kt
@@ -8,6 +8,11 @@ import org.thymeleaf.context.Context
 inline fun <reified T> T.getLogger(): Logger = LoggerFactory.getLogger(T::class.java)
 
 object Utiles {
+
+    /**
+     * drools template 파일을 템플릿 엔진으로 렌더링 후 결과 문자열 반환
+     * @param templateName templates 폴더 내 .drl 템플릿 파일명 (확장자 제외)
+     * **/
     //<editor-fold desc="thymeleaf to drl 문자열 생성">
     fun generateDrlFromTemplate(
         text: String ,
