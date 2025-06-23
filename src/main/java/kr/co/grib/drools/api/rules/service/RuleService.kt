cacdc86ca8ef7b448a947c88e29ce0617812afcb
smj1Grib <smj1@grib.co.kr>
2025-06-23 19:42:28 +0900

    [수정/추가] Rule 수정, 추가 ,삭제 , 실행 로직 완료

--- src/main/java/kr/co/grib/drools/api/rules/service/RuleService.kt
+++ src/main/java/kr/co/grib/drools/api/rules/service/RuleService.kt
@@ -12,12 +12,21 @@ interface RuleService {
     fun doPostCreateRule(req: RuleCreateRequestDto):BaseCtlDto
     //</editor-fold desc="[POST] /create Drool 생성">
 
-    //<editor-fold desc="[POST] /addRule rule 추가">
-    fun doPostAddRule(req: RuleAddRequestDto):BaseCtlDto
-    //</editor-fold desc="[POST] /addRule rule 추가">
+    //<editor-fold desc="[PATCH] /addRule rule 추가">
+    fun doPatchAddRule(req: RuleAddRequestDto):BaseCtlDto
+    //</editor-fold desc="[PATCH] /addRule rule 추가">
 
-//    fun doEvaluateRules(req: RuleRequestDto): RuleResponseCtlDto
+    //<editor-fold desc="[DELETE] /deleteRule rule 삭제">
+    fun doDeleteRule(ruleId: Int, ruleName: List<String>):BaseCtlDto
+    //</editor-fold desc="[DELETE] /deleteRule rule 삭제">
 
-    fun doEvaluateRulesT(req: RuleRequestDto): RuleResponseCtlDto
+    //<editor-fold desc="[DELETE] /remove rule 자체 삭제">
+    fun doRemoveRule(ruleId: Int):BaseCtlDto
+    //</editor-fold desc="[DELETE] /remove rule 자체 삭제">
+
+    //<editor-fold desc="[POST] /execute rule 파일 실행">
+    fun doPostExecute(req: RuleRequestDto) : RuleResponseCtlDto
+    //</editor-fold desc="[POST] /execute rule 파일 실행">
+
 
 }
