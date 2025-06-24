89cb388f6eabc46430818782632d7a975ee232ab
smj1Grib <smj1@grib.co.kr>
2025-06-24 12:38:16 +0900

    [수정/추가] 실행 로직 에 history 내역 적재 추가

--- src/main/java/kr/co/grib/drools/api/rules/repository/DroolsAuditHistoryRepo.kt
+++ src/main/java/kr/co/grib/drools/api/rules/repository/DroolsAuditHistoryRepo.kt
@@ -1,17 +1,42 @@
 package kr.co.grib.drools.api.rules.repository
 
-import com.querydsl.core.QueryFactory
 import com.querydsl.jpa.impl.JPAQueryFactory
+import jakarta.persistence.EntityManager
+import kr.co.grib.drools.Entity.DroolsAuditHistory
 import kr.co.grib.drools.Entity.QDroolsAuditHistory
+import kr.co.grib.drools.api.rules.dto.RuleAuditHistoryDto
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
 
 @Repository
 class DroolsAuditHistoryRepo (
-    private val queryFactory: JPAQueryFactory
+    private val queryFactory: JPAQueryFactory,
+    private val em: EntityManager
 ) {
     private val droolsAuditHistory = QDroolsAuditHistory.droolsAuditHistory
 
     //<editor-fold desc="History 등록">
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    fun saveDroolsAuditHistory(
+        req: RuleAuditHistoryDto
+    ){
+        val ruleHistory = DroolsAuditHistory(
+            ruleName = req.ruleName,
+            ruleAuditIp = req.ruleAditIp,
+            ruleApiMethod = req.ruleApiMethod,
+            ruleApi = req.ruleApi,
+            ruleFact = req.ruleFact,
+            ruleResult = req.ruleResult,
+            status = req.ruleAditStatus,
+            message = req.ruleMsg,
+            createdBy = req.ruleName,
+            createdAt = LocalDateTime.now()
+        )
+        em.persist(ruleHistory)
+        em.flush()
+    }
     //</editor-fold desc="History 등록">
 
 }
